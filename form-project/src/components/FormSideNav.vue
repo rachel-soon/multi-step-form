19e6407fe52f183b569e21a1f37a4927d1b53df3
rachel-soon <rachel_may0106@hotmail.com>
2023-10-06 16:28:40 +0800

    feat: delete some files

---
+++ form-project/src/components/FormSideNav.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>sdfjsd</div>
+</template>
---
+++ form-project/src/views/SubscriptionForm.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import FormSideNav from '@/components/FormSideNav.vue'
+
+</script>
+
+<template>
+    <FormSideNav></FormSideNav>
+</template>
