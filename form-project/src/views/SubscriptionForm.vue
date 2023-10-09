1a1c33b8ccb85388d4bb9398896cebdad09c1139
rachel-soon <rachel_may0106@hotmail.com>
2023-10-09 14:34:01 +0800

    feat: implement basic color scheme

--- form-project/src/views/SubscriptionForm.vue
+++ form-project/src/views/SubscriptionForm.vue
@@ -45,21 +45,21 @@ const getTitle = computed(() => {
 const getSubtitle = computed(() => {
   switch (currentStep.value) {
     case 1:
-      return 'Please provide your name, email address, and phone number'
+      return 'Please provide your name, email address, and phone number.'
     case 2:
-      return 'You have the option of monthly or yearly billing'
+      return 'You have the option of monthly or yearly billing.'
     case 3:
-      return 'Add-ons help enhance your gaming experience'
+      return 'Add-ons help enhance your gaming experience.'
     case 4:
-      return 'Double-check everything looks OK before confirming'
+      return 'Double-check everything looks OK before confirming.'
   }
 })
 </script>
 
 <template>
-  <div class="bg-white w-8/12 rounded-xl grid grid-cols-3 form-body p-5">
+  <div class="form-body">
     <FormSideNav class="form-body__nav"></FormSideNav>
-    <div class="col-span-2 form-body__form pt-8 pl-10">
+    <div class="col-span-2 form-body__form pt-8 px-10">
       <div class="form-body__heading">
         <div class="form-body__title">{{ getTitle }}</div>
         <div class="form-body__subtitle">{{ getSubtitle }}</div>
@@ -67,12 +67,12 @@ const getSubtitle = computed(() => {
 
       <component :is="getComponent"></component>
       <div class="form-body__footer">
-        <div><button @click="previousStep">Go Back</button></div>
-        <div><button @click="nextStep">Next Step</button></div>
+        <div><button v-if="currentStep !== 1" @click="previousStep">Go Back</button></div>
+        <div class="flex justify-end">
+          <button v-if="currentStep !== 4" @click="nextStep">Next Step</button>
+          <button v-else>Confirm</button>
+        </div>
       </div>
     </div>
-    <!-- <button class="border-2" @click="nextStep">Increase step</button>
-    <button class="border-2" @click="previousStep">Decrease step</button>
-    <div>{{ currentStep }}</div> -->
   </div>
 </template>
--- form-project/tailwind.config.js
+++ form-project/tailwind.config.js
@@ -3,7 +3,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#02295a',
+        secondary: '#473dff',
+        muted: '#9699ab'
+      }
+    }
   },
   plugins: []
 }
