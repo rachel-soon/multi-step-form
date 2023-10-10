415e66907ea67e4f4d25aeae4c0eba16ed773697
rachel-soon <rachel_may0106@hotmail.com>
2023-10-10 17:06:38 +0800

    feat: implement plan componetn

--- form-project/src/components/SubscriptionFormPlan.vue
+++ form-project/src/components/SubscriptionFormPlan.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
 import BaseCard from './Base/BaseCard.vue'
+import { reactive } from 'vue'
+import arcadeIcon from '@/assets/img/icon-arcade.svg'
+import advancedIcon from '@/assets/img/icon-advanced.svg'
+import proIcon from '@/assets/img/icon-pro.svg'
+const plans = reactive([
+  {
+    name: 'Arcade',
+    monthlyPrice: 9,
+    yearlyPrice: 90,
+    icon: arcadeIcon
+  },
+  {
+    name: 'Advanced',
+    monthlyPrice: 12,
+    yearlyPrice: 120,
+    icon: advancedIcon
+  },
+  {
+    name: 'Pro',
+    monthlyPrice: 15,
+    yearlyPrice: 150,
+    icon: proIcon
+  }
+])
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-4">
-    <BaseCard>
+    <BaseCard v-for="plan in plans" :key="plan.name">
       <div class="mb-10 mt-2">
-        <img src="@/assets/img/icon-arcade.svg" alt="arcade" />
+        <img :src="plan.icon" :alt="plan.name" />
       </div>
-
-      <div class="plan__title">Arcade</div>
-      <div class="plan__price text-muted text-sm">$9/mo</div>
+      <div class="plan__title">{{ plan.name }}</div>
+      <div class="plan__price text-muted text-sm">${{ plan.monthlyPrice }}/mo</div>
     </BaseCard>
-    <BaseCard></BaseCard>
-    <BaseCard></BaseCard>
-    <div class="col-span-3 bg-magnolia w-full mt-2 rounded-md p-3">sdfsdf</div>
+
+    <div class="col-span-3 bg-magnolia w-full mt-2 rounded-md p-3"></div>
   </div>
 </template>
 
